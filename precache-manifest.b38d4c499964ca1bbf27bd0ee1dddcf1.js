251ae09eff89140eb18929a060642c98268aff8f
TT ZZ <zolman99@gmail.com>
2020-02-20 17:38:06 +0300

    Updates

---
+++ precache-manifest.b38d4c499964ca1bbf27bd0ee1dddcf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8aa470016948aa46cae9ba05382eb72",
+    "url": "/cvssv3-calculator/index.html"
+  },
+  {
+    "revision": "c70e44d576963185eaa6",
+    "url": "/cvssv3-calculator/static/css/main.b0d1f5b8.chunk.css"
+  },
+  {
+    "revision": "8d6b9e15aa62b7bca3aa",
+    "url": "/cvssv3-calculator/static/js/2.a6a75789.chunk.js"
+  },
+  {
+    "revision": "c70e44d576963185eaa6",
+    "url": "/cvssv3-calculator/static/js/main.fd8a1323.chunk.js"
+  },
+  {
+    "revision": "b71ce21f9be02a56c111",
+    "url": "/cvssv3-calculator/static/js/runtime-main.b247394a.js"
+  }
+]);
